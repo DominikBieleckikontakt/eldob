57e0d2b011fd8e2225393aa38a7bff7a3440f535
Dominik <dominikbielecki.kontakt@gmail.com>
2024-09-12 09:46:18 +0200

    navbar mostly made, scss and bootstrap installed

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,24 +3,34 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   <title>{{ $title }}</title>
+  
+
+  @vite(['public/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-  <nav>
-    <a href="/" class="logo__container">
-      <img alt="Logo Eldob" src="{{ asset('img/logo.png') }}" class="logo">
-    </a>
-    <ul>
-      <li><a href="">Oferta</a></li>
-      <li><a href="">Sprzedaż</a></li>
-      <li><a href="">Realizacje</a></li>
-      <li><a href="">Referencje</a></li>
-      <li><a href="">Czym się zajmujemy</a></li>
-      <li><a href="">Kontakt</a></li>
-      <li><a href="" class="link__btn">Wycena</a></li>
-    </ul>
-  </nav>
-  {{ $slot }}
+  <nav class="navbar navbar-expand-lg">
+    <div class="container-fluid nav__container">
+      <a href="/" class="logo__container navbar-brand">
+        <img alt="Logo Eldob" src="{{ asset('img/logo.png') }}" class="logo">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <x-nav-link href="#" class="a__link {{ request()->is('/') ? 'active' : '' }}">Oferta</x-nav-link>
+          <x-nav-link href="#" class="a__link {{ request()->is('/sales') ? 'active' : '' }}">Sprzedaż</x-nav-link>
+          <x-nav-link href="#" class="a__link {{ request()->is('/realizations') ? 'active' : '' }}">Realizacje</x-nav-link>
+          <x-nav-link href="#" class="a__link {{ request()->is('/what') ? 'active' : '' }}">Czym się zajmujemy</x-nav-link>
+          <x-nav-link href="#" class="a__link {{ request()->is('/contact') ? 'active' : '' }}">Kontakt</x-nav-link>
+          <x-nav-link href="#" class="a__link {{ request()->is('/pricing') ? 'active' : '' }}">Wycena</x-nav-link>
+        </ul>
+      </div>
+    </nav>
+    </div>
+    <main>
+      {{ $slot }}
+    </main>
 </body>
 </html>
